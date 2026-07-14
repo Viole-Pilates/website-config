55dbb5c9be2f7eb064f5252401980a2af2cc8023
Fidel <fidel@Fidels-MacBook-Pro.local>
2026-07-14 22:01:02 +0200

    Fixed env variable again

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -38,7 +38,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer',${GTM_ID});`,
+})(window,document,'script','dataLayer','${GTM_ID}');`,
           }}
         />
       </head>
